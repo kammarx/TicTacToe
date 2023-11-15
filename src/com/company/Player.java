04579937a16d5e4125c7789dfcec36a697df5b8f
kammar <kammarufa6@gmail.com>
2023-11-15 17:52:58 +0100

    class PLayer implemented

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,5 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Player {
+    private String name;
+    private char mark;
+
+    public Player(String name, char mark){
+        this.name = name;
+        this.mark = mark;
+    }
+
+    public char getMark(){
+        return mark;
+    }
+
+    public int[] makeMove(){
+        Scanner sc = new Scanner(System.in);
+        int[] move = new int[2];
+        System.out.print(name + " , enter your move: ");
+        move[0] = sc.nextInt();
+        move[1] = sc.nextInt();
+        return move;
+    }
 
 }
